fa8b1e2aab853b264080ea62c58cddf71c06e33e
Paul Jubilee Owuoth <pauljubilee50@gmail.com>
2024-10-01 20:13:35 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+  }
+  const record = [
+    { year: 1998, result: "W" },
+    { year: 2005, result: "L" },
+    { year: 2016, result: "W" },
+  ];
+  console.log(superbowlWin(record));
